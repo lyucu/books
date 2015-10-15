67aed656549eb30f55f92f7fd97c11e072850ac6
MarcusLiu <yuchun.liu@otms.cn>
2015-10-15 13:46:15 +0800

    ｆｄｓ

--- JavaSe8/src/main/java/stream/test/LT2.java
+++ JavaSe8/src/main/java/stream/test/LT2.java
@@ -1,11 +1,41 @@
 package stream.test;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class LT2 {
     public static void main(String... args) {
-        System.out.println(123);
+        LT2 t = new LT2();
+        t.test3();
     }
 
     public void test1() {
+        String[] source = "123010203012f0d0d0skoj0a0f0j0pa80900021370731fsd0fkhja09123120".split("0");
+        System.out.println(Arrays.toString(source));
+        Stream<String> ss = Stream.of(source);
+        Arrays.asList(source).parallelStream();
+        Stream<String> l = ss.parallel().filter(word -> {
+            System.out.println("==");
+            return word.length() > 0;
+        }).limit(1);
+        System.out.println(l.count());
+        ;
+    }
+
+    public void test2() {
+        int bd = 1000;
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        Stream<Integer> xx = Stream.iterate(bd, f -> (a * f + b) % c);
+        xx.limit(3).forEach(l -> System.out.println(l));
+    }
+
+    public void test3() {
+        String source = "12312fdkfsadpdjfdjp123uifdjkpsafdj";
+        IntStream stera = IntStream.iterate(0, f -> f + 1).limit(source.length() - 1);
+        stera.parallel().forEach(l -> System.out.println(source.charAt(l)));
 
     }
 }
