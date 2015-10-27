faa9c6dc963449f2c651476bee054d4767163003
MarcusLiu <yuchun.liu@otms.cn>
2015-10-27 13:57:43 +0800

    123

--- JavaSe8/src/main/java/nashorn/Demo.java
+++ JavaSe8/src/main/java/nashorn/Demo.java
@@ -1,5 +1,10 @@
 package nashorn;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -19,10 +24,22 @@ public class Demo {
 
     @Test
     // 调用js脚本
-    public void test2() throws ScriptException {
+    public void test2() throws ScriptException, IOException {
         ScriptEngineManager sm = new ScriptEngineManager();
         ScriptEngine engine = sm.getEngineByName("nashorn");
         Object result = engine.eval("'Hellow Wolred'");
         System.out.println(result);
+        URL url = getClass().getClassLoader().getResource("jsFile.js");
+        result = engine.eval(Files.newBufferedReader(Paths.get(url.getPath())));
+        System.out.println(result);
+
+        url = getClass().getClassLoader().getResource("jsFile1.js");
+        result = engine.eval(Files.newBufferedReader(Paths.get(url.getPath())));
+        System.out.println(result);
+
+    }
+
+    @Test
+    public void test3() throws ScriptException {
     }
 }
