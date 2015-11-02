7b0ce788ddb1629a330df7e86df792f36762b930
MarcusLiu <yuchun.liu@otms.cn>
2015-11-02 12:57:51 +0800

    123

--- JavaSe8/src/main/java/newinjava8/Demo.java
+++ JavaSe8/src/main/java/newinjava8/Demo.java
@@ -2,8 +2,10 @@ package newinjava8;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -11,9 +13,31 @@ import org.junit.Test;
 public class Demo {
     @Test
     public void test() throws IOException {
-        URL url = getClass().getClassLoader().getResource("jsFile.js");
+        URL url = getClass().getClassLoader().getResource("jsFile1.js");
 
         Stream<String> stream = Files.lines(Paths.get(url.getPath()));
+        stream.forEach(System.out::println);
+
+        // 遍历目录
+        // Files.list(dir);
+        // Files.walk(start, options);
+    }
+
+    @Test
+    public void test2() {
+        Base64.Encoder encoder = Base64.getEncoder();
+        String original = "test";
+        String ecdeoed = encoder.encodeToString(original.getBytes(StandardCharsets.UTF_8));
+        System.out.println(ecdeoed);
+        // encoder.wrap();
+
+    }
+
+    // 1.7
+    @Test
+    public void test3() {
+        // List<String> list = Files.readAllLines(path);
+        // Files.write(path, bytes, options);
 
     }
 }
