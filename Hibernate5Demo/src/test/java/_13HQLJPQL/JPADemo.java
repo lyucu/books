91563165ce124d40844510ce4571618d189e5e16
MarcusLiu <yuchun.liu@otms.cn>
2015-12-03 13:57:44 +0800

    start 13.5

--- Hibernate5Demo/src/test/java/_13HQLJPQL/JPADemo.java
+++ Hibernate5Demo/src/test/java/_13HQLJPQL/JPADemo.java
@@ -36,6 +36,65 @@ public class JPADemo {
         String hql5 = "select * from Book book where book.auther.id  =  123";
     }
 
+    @Test
+    public void collectionMember() {
+        String hql1 = "select * form Customer c join c.orders o where o.status = 1";
+        // same as
+        String hql2 = "select * from Cusotmer c in (c.orders) o where o.status = 1";
+        // value
+        String hql3 = "select i from Product p join p.imagers i where p.id = 123";
+        String hql4 = "select value(i) from Product p join p.images i where p.id = 123";
+
+        // key
+        String hql5 = "select key(i) from Product p join p.imagers i where p.id = 123";
+        String hql6 = "select entry(i) from Product p join p.imagers i where p.id = 123";
+        String hql7 = "select sum(i) from Product p join p.imagers i where p.id = 123";
+    }
+
+    @Test
+    public void expression() {
+        String hql1;
+        hql1 = "select *c form Cutomer c where c.name = '123''123'";
+        hql1 = "select o from Order o where o.num = 123";
+        hql1 = "select o from Order o where o.num = 123L";
+        hql1 = "select o from Order o where o.num = 123.0";
+        hql1 = "select o from Order o where o.num = 123.F";
+        hql1 = "select o from Order o where o.num = 123.5e+3";
+        hql1 = "select o from Order o where o.num = 123.5e+3F";
+    }
+
+    @Test
+    public void parameters() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        String sql = "update Book b set b.title = ?!";
+        entityManager.createQuery(sql).setParameter(1, "t1").executeUpdate();
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+    }
+
+    @Test
+    public void type() {
+        String hql = "select p from Payment p where type(p) = CreditCardPayment";
+    }
+
+    @Test
+    public void caseExpression() {
+        String hql;
+        hql = "select case c.nickName when null then '<no name>' else c.nickName end from Customer c";
+
+        hql = "select nvl(c.nickname , 'no name') from CustomerName";
+        hql = "select isnull(c.nickname, 'no name') from Customer o";
+        hql = " select coalesce(c.nickName,'no name') from Customer o";
+
+        hql = " select case when c.name is not null then c.name when c.nickName is not null then c.nickName else 'no name' end from Customer c";
+        hql = " select coalesce(c.name , c.nickName, 'no name') from Customer c";
+
+        hql = "select nullif (c.name , c.nickName) from Customer c";
+        hql = "select case when c.name = c.nickName then null else c.name end from Customer c";
+    }
+
     @Before
     public void load() {
         try {
--- Hibernate5Demo/src/test/java/_13HQLJPQL/NativeDemo.java
+++ Hibernate5Demo/src/test/java/_13HQLJPQL/NativeDemo.java
@@ -47,6 +47,75 @@ public class NativeDemo {
     }
 
     @Test
+    public void collectionMember() {
+        String hql1 = "select * form Customer c join c.orders o where o.status = 1";
+        // same as
+        String hql2 = "select * from Cusotmer c in (c.orders) o where o.status = 1";
+        // value
+        String hql3 = "select i from Product p join p.imagers i where p.id = 123";
+        String hql4 = "select value(i) from Product p join p.images i where p.id = 123";
+
+        // key
+        String hql5 = "select key(i) from Product p join p.imagers i where p.id = 123";
+        String hql6 = "select entry(i) from Product p join p.imagers i where p.id = 123";
+        String hql7 = "select sum(i) from Product p join p.imagers i where p.id = 123";
+    }
+
+    @Test
+    public void expression() {
+        String hql1;
+        hql1 = "select *c form Cutomer c where c.name = '123''123'";
+        hql1 = "select o from Order o where o.num = 123";
+        hql1 = "select o from Order o where o.num = 123L";
+        hql1 = "select o from Order o where o.num = 123.0";
+        hql1 = "select o from Order o where o.num = 123.F";
+        hql1 = "select o from Order o where o.num = 123.5e+3";
+        hql1 = "select o from Order o where o.num = 123.5e+3F";
+    }
+
+    @Test
+    public void parameters() {
+        try (Session session = sessionFactory.openSession()) {
+            String sql = "update Book b set b.title = ?1";
+            session.createQuery(sql).setString(1, "title").executeUpdate();
+        }
+
+    }
+
+    @Test
+    public void collectionExpression() {
+        String hql;
+        hql = "select cal form calendar cal where maxelemtn(cal.holidays) > currnet_date()";
+        hql = "selec ... maxindex(cal.items) > ";
+        hql = "selec ... minelement(o.items) where";
+        hql = "select .. where kit in elements (m.kittends)";
+        hql = "select ... hwere cal.items[0].id";
+        hql = "select ... hwere cal.items['nandday'].id";
+        hql = "select ... hwere cal.items[ o.delivery[0]].id";
+    }
+
+    @Test
+    public void type() {
+        String hql = "select p from Payment p where type(p) = CreditCardPayment";
+    }
+
+    @Test
+    public void caseExpression() {
+        String hql;
+        hql = "select case c.nickName when null then '<no name>' else c.nickName end from Customer c";
+
+        hql = "select nvl(c.nickname , 'no name') from CustomerName";
+        hql = "select isnull(c.nickname, 'no name') from Customer o";
+        hql = " select coalesce(c.nickName,'no name') from Customer o";
+
+        hql = " select case when c.name is not null then c.name when c.nickName is not null then c.nickName else 'no name' end from Customer c";
+        hql = " select coalesce(c.name , c.nickName, 'no name') from Customer c";
+
+        hql = "select nullif (c.name , c.nickName) from Customer c";
+        hql = "select case when c.name = c.nickName then null else c.name end from Customer c";
+    }
+
+    @Test
     public void fetchByporfiles() {
         try (Session session = sessionFactory.openSession()) {
             Book book = (Book) session.byId(Book.class).load(1l);
