1db0f2f626fc5c2f04bb130dbb44862f0ef7225d
MarcusLiu <yuchun.liu@otms.cn>
2015-12-30 11:57:42 +0800

    finish hibernate API

--- Hibernate5Demo/src/main/java/mapping/Produce.java
+++ Hibernate5Demo/src/main/java/mapping/Produce.java
@@ -3,6 +3,8 @@ package mapping;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 
 @Entity
@@ -17,4 +19,20 @@ public class Produce {
     @Basic
     @Column(name = "NOTES")
     private String description;
+
+    @Enumerated(EnumType.STRING)
+    public Tp tp;
+
+    @Enumerated(EnumType.ORDINAL)
+    public Tp tp1;
+}
+
+enum Tp {
+    ONE(1), TWO(2);
+    private final int count;
+
+    private Tp(int count) {
+        this.count = count;
+    }
+
 }
