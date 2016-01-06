b419db6adb27304a8282ed2c853041f1bdf5903a
MarcusLiu <yuchun.liu@otms.cn>
2016-01-06 13:58:34 +0800

    fix mybatis demo

--- mybatis/src/test/java/com/lyc/mybatis/Test.java
+++ mybatis/src/test/java/com/lyc/mybatis/Test.java
@@ -1,12 +1,17 @@
 package com.lyc.mybatis;
 
+import java.io.IOException;
 import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class Test {
 
     @org.junit.Test
-    public void testLoad() {
-        String resource = "org/mybatis/example/mybatis-config.xml";
+    public void testLoad() throws IOException {
+        String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
     }
