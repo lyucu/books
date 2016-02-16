0356d1d1b2608f887f80672a753f0873b1fd3151
ycliu <ycliu@pdeslindahan>
2016-02-16 15:30:54 +0800

    finishi tutil 3

--- rabbitMq/rabbitMq/src/test/java/com/lyc/rabbitMq/Tutor.java
+++ rabbitMq/rabbitMq/src/test/java/com/lyc/rabbitMq/Tutor.java
@@ -120,20 +120,60 @@ public class Tutor extends TestCase {
     }
 
     @Test
-    public void testExchange() throws IOException, TimeoutException {
-        durable = true;
+    public void testExchangeProducer() throws IOException, TimeoutException {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("localhost");
         Connection connection = connectionFactory.newConnection();
         Channel channel = connection.createChannel();
 
-        // make queue durable
-        channel.queueDeclare("demoQueue", durable, false, false, null);
+        //direct, topic, headers , fanout for exchange type
+        //fanout broadcast message to every queue
+        channel.exchangeDeclare("demoExchange", "fanout");
+        
         String message = "hellow world1";
-        // make message durable
-        channel.basicPublish("", "demoQueue", MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
+        //demoExchange is exchange name
+        channel.basicPublish("demoExchange", "", null, message.getBytes());
         channel.close();
         connection.close();
 
+    }
+    
+    @Test
+    public void testExchangeProducerConsumer() throws IOException, TimeoutException{
+    	
+         ConnectionFactory connectionFactory = new ConnectionFactory();
+         connectionFactory.setHost("localhost");
+         Connection connection = connectionFactory.newConnection();
+         final Channel channel = connection.createChannel();
+         
+         channel.exchangeDeclare("demoExchange", "fanout");
+         
+         //creat temp queue
+         String queueName = channel.queueDeclare().getQueue();
+         // bind the queue and the exchange for consumer
+         channel.queueBind(queueName, "demoExchange", "");
+         System.out.println(queueName);
+
+         // 设置consumer能接受的message的数量
+         channel.basicQos(1);
+
+         Consumer consumer = new DefaultConsumer(channel) {
+             @Override
+             public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
+                     throws IOException {
+                 String message = new String(body, "UTF-8");
+                 System.out.println(message);
+             }
+         };
+         channel.basicConsume(queueName, true, consumer);
+         while (true) {
+        	 try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+        	 System.out.println("2");
+         }
+         
     }
 }
