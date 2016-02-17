184f0ae56487157d3c5bbb69fc4a6ddf889829f1
ycliu <ycliu@pdeslindahan>
2016-02-17 14:09:41 +0800

    finish tutil 5

--- rabbitMq/rabbitMq/src/test/java/com/lyc/rabbitMq/Tutor.java
+++ rabbitMq/rabbitMq/src/test/java/com/lyc/rabbitMq/Tutor.java
@@ -62,8 +62,6 @@ public class Tutor extends TestCase {
 
         // false 需要ackflag true 自动删除
         channel.basicConsume("demoQueue", false, consumer);
-        while (true) {
-        }
     }
 
     @Test
@@ -115,8 +113,6 @@ public class Tutor extends TestCase {
         };
         // false 需要ackflag true 自动删除
         channel.basicConsume("demoQueue", false, consumer);
-        while (true) {
-        }
     }
 
     @Test
@@ -175,5 +171,132 @@ public class Tutor extends TestCase {
         	 System.out.println("2");
          }
          
+    }
+    
+    
+    @Test
+    public void testRoutingProducer() throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        //direct, topic, headers , fanout for exchange type
+        channel.exchangeDeclare("demoExchange", "direct");
+        
+        String severity = "sev1";
+        String message = "hellow world1";
+        //demoExchange is exchange name and severity is route_key
+        channel.basicPublish("demoExchange", severity, null, message.getBytes());
+        
+         severity = "sev2";
+         message = "hellow world2";
+        //demoExchange is exchange name and severity is route_key
+        channel.basicPublish("demoExchange", severity, null, message.getBytes());
+        
+        channel.close();
+        connection.close();
+    }
+    
+    @Test
+    public void  testRoutingConsumer () throws IOException, TimeoutException, InterruptedException {
+    	ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        Connection connection = connectionFactory.newConnection();
+        final Channel channel = connection.createChannel();
+        
+        channel.exchangeDeclare("demoExchange", "direct");
+        
+        //creat temp queue
+        String queueName = channel.queueDeclare().getQueue();
+        String severity = "sev2";
+        // bind the queue and the exchange for consumer  severity is route_key 可以用for循环连续绑定
+        channel.queueBind(queueName, "demoExchange", severity);
+        System.out.println(queueName);
+
+        // 设置consumer能接受的message的数量
+        channel.basicQos(1);
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(message);
+            }
+        };
+        channel.basicConsume(queueName, true, consumer);
+        while (true) {
+        	Thread.sleep(3000);
+       	 	System.out.println(severity);
+        }
+    }
+    
+    @Test
+    public void testTopicProducer() throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        //direct, topic, headers , fanout for exchange type
+        channel.exchangeDeclare("demoExchange", "topic");
+        
+        String routekey = "sev1.test2";
+        String message = "hellow world1";
+        //routekey is route_key
+        channel.basicPublish("demoExchange", routekey, null, message.getBytes());
+        
+        routekey = "sev2.test1";
+         message = "hellow world2";
+        //demoExchange is exchange name and severity is route_key
+        channel.basicPublish("demoExchange", routekey, null, message.getBytes());
+        
+        routekey = "sev2.test2";
+        message = "hellow world3";
+       //demoExchange is exchange name and severity is route_key
+       channel.basicPublish("demoExchange", routekey, null, message.getBytes());
+        
+        channel.close();
+        connection.close();
+    }
+    
+    @Test
+    public void  testTopicConsumer () throws IOException, TimeoutException, InterruptedException {
+    	ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        Connection connection = connectionFactory.newConnection();
+        final Channel channel = connection.createChannel();
+        
+        channel.exchangeDeclare("demoExchange", "topic");
+        
+        //creat temp queue
+        String queueName = channel.queueDeclare().getQueue();
+        String severity = "sev1.*";
+        // bind the queue and the exchange for consumer  severity is route_key 可以用for循环连续绑定
+        channel.queueBind(queueName, "demoExchange", severity);
+        
+         severity = "*.test1";
+        // bind the queue and the exchange for consumer  severity is route_key 可以用for循环连续绑定
+        channel.queueBind(queueName, "demoExchange", severity);
+        
+        System.out.println(queueName);
+
+        // 设置consumer能接受的message的数量
+        channel.basicQos(1);
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(message);
+            }
+        };
+        channel.basicConsume(queueName, true, consumer);
+        while (true) {
+        	Thread.sleep(3000);
+       	 	System.out.println(severity);
+        }
     }
 }
